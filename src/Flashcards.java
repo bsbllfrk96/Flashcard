1fede339917b8946140ee4b7561c70150621a9ed
bsbllfrk96 <john.joseph@rockets.utoledo.edu>
2015-03-01 17:14:37 -0500

    Initial test

---
+++ src/Flashcards.java
@@ -0,0 +1,9 @@
+
+public class Flashcards {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Flashcards1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Flashcards1 {
+	
+
+	public static void main(String[] args) {
+		Scanner stdin = new Scanner(System.in);
+		String input = null;
+		System.out.println("This is a test");
+		input = stdin.nextLine();
+		System.out.println(input);
+		stdin.close();
+	}
+
+}
